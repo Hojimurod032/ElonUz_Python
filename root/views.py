f32507b398360f68bccd25e4e309a182d97df2f4
a312xa <hojimurod832@gmail.com>
2026-07-09 15:37:26 +0500

    Views

--- root/views.py
+++ root/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeViewList(TemplateView):
+    template_name = 'Home.html'
+
+
+class AuthViewList(TemplateView):
+    template_name = 'Auth.html'
+
+
+class DetailViewList(TemplateView):
+    template_name = 'Detail.html'
+
+
+class ProfileViewList(TemplateView):
+    template_name = 'Profile.html'
